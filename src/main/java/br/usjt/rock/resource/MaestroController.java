2b669963454b9866f5589d4d5ba7d3d1f8ad751f
Renan <renan.viana@vilt-group.com>
2019-06-01 20:54:04 -0300

    foi refatorado todos os métodos de requisições

--- src/main/java/br/usjt/rock/resource/MaestroController.java
+++ src/main/java/br/usjt/rock/resource/MaestroController.java
@@ -1,125 +1,47 @@
 package br.usjt.rock.resource;
 
-import java.util.Map;
+import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UrlPathHelper;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
+import br.usjt.rock.model.bean.ApiEnum;
 
 @RestController
 @RequestMapping("/maestro/api/**")
-public class MaestroController {
+public class MaestroController extends RestClient {
 	
-	// TODO: Pegar de variavel de ambiente
-		// TODO: Deixar variavel nome da aplicacao
-		
-		private static String MAESTRO_URL = "http://maestro.lucasduarte.club";
-		RestTemplate restTemplate = new RestTemplate();
-		ObjectMapper mapper = new ObjectMapper();
+	private static final String MAESTRO_URL = "http://maestro.lucasduarte.club";
 
-		@GetMapping
-		public ResponseEntity<?> getRequest(HttpServletRequest request, HttpServletResponse response, Object handler)
-				throws Exception {
-			String param = "?";
-			Map<String, String[]> parameters = request.getParameterMap();
+	public MaestroController() {
+		super(ApiEnum.MAESTRO, MAESTRO_URL);
+	}
 
-			// Get Parameters of String
-			for (Object key : parameters.keySet()) {
-				String keyStr = (String) key;
-				String[] value = (String[]) parameters.get(keyStr);
-				param += "" + ((String) key + "=" + String.join("", value) + "&");
-			}
-			param = param.substring(0, param.length() - 1);
+	@GetMapping
+	public ResponseEntity<?> get(HttpServletRequest request) throws IOException {
+		return getRequest(request);
+	}
 
-			String path = new UrlPathHelper().getPathWithinApplication(request);
-			path = path.replaceAll("/maestro", "");
-			String urlRequest = MAESTRO_URL + path;
-
-			// Check if params exists
-			if (param != "?") {
-				urlRequest = MAESTRO_URL + path + param;
-			}
-
-			try {
-				ResponseEntity<String> responseServer = restTemplate.getForEntity(urlRequest, String.class);
-				JsonNode formatedJson = mapper.readTree(responseServer.getBody());
-				return ResponseEntity.status(responseServer.getStatusCode()).body(formatedJson);
-
-			} catch (HttpClientErrorException e) {
-				JsonNode formatedJson = mapper.readTree(e.getResponseBodyAsString());
-				return ResponseEntity.status(e.getStatusCode()).body(formatedJson);
-			}
-		}
-
-		@PostMapping
-		public ResponseEntity<?> postRequest(@RequestBody String body, HttpServletRequest request,
-				HttpServletResponse response) throws Exception {
-
-			String path = new UrlPathHelper().getPathWithinApplication(request);
-			path = path.replaceAll("/maestro", "");
-			String urlRequest = MAESTRO_URL + path;
-
-			JsonNode formatedJson = mapper.readTree(body);
-			try {
-				String responseServer = restTemplate.postForObject(urlRequest, formatedJson, String.class);
-				JsonNode formatedResponse = mapper.readTree(responseServer);
-				return ResponseEntity.status(response.getStatus()).body(formatedResponse);
-
-			} catch (HttpClientErrorException e) {
-				JsonNode formatedResponse = mapper.readTree(e.getResponseBodyAsString());
-				return ResponseEntity.status(response.getStatus()).body(formatedResponse);
-			}
-		}
-
-		@PutMapping
-		public ResponseEntity<?> putRequest(@RequestBody (required=false) String body, HttpServletRequest request,
-				HttpServletResponse response) throws Exception {
-
-			String path = new UrlPathHelper().getPathWithinApplication(request);
-			path = path.replaceAll("/maestro", "");
-			String urlRequest = MAESTRO_URL + path;
-			
-			System.out.println(urlRequest);
-			try {
-				HttpHeaders headers = new HttpHeaders();
-				headers.setContentType(MediaType.APPLICATION_JSON);
-				HttpEntity<String> requestEntity = new HttpEntity<>(body, headers);	
-				
-				ResponseEntity<String> responseServer = restTemplate.exchange(urlRequest, HttpMethod.PUT, requestEntity,
-						String.class);
-				
-				JsonNode formatedJson;
-				try {
-					formatedJson = mapper.readTree(responseServer.getBody());
-				} catch (Exception e) {
-					System.out.println("Entrou exception");
-					ObjectNode objectNode1 = mapper.createObjectNode();
-					objectNode1.put("Status", "True");
-					return ResponseEntity.status(responseServer.getStatusCode()).body(objectNode1);
-				}
-				return ResponseEntity.status(responseServer.getStatusCode()).body(formatedJson);
-
-			} catch (HttpClientErrorException e) {
-				JsonNode formatedResponse = mapper.readTree(e.getResponseBodyAsString());
-				return ResponseEntity.status(response.getStatus()).body(formatedResponse);
-			}
-		}
+	@PostMapping
+	public ResponseEntity<?> post(@RequestBody String body, HttpServletRequest request) throws IOException {
+		return postRequest(body, request);
+	}
+	
+	@PutMapping
+	public ResponseEntity<?> put(@RequestBody (required=false) String body, HttpServletRequest request) throws IOException {
+		return putRequest(body, request);
+	}
+	
+	@DeleteMapping
+	public ResponseEntity<?> delete(HttpServletRequest request) throws IOException {
+		return deleteRequest(request);
+	}
 }
