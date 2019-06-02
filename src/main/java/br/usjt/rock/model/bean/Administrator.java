2e76fc87beb2946a43fffb9eba1b02021127d0bd
Renan <renan.viana@vilt-group.com>
2019-06-01 22:51:22 -0300

    foi adicionado atributo nome à classe

--- src/main/java/br/usjt/rock/model/bean/Administrator.java
+++ src/main/java/br/usjt/rock/model/bean/Administrator.java
@@ -1,56 +1,99 @@
 package br.usjt.rock.model.bean;
 
-import java.io.Serializable;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.io.Serializable;
 
 @Entity
 @Table(name = "tb_administrador")
+@JsonIgnoreProperties(value = { "senha" }, allowSetters = true)
 public class Administrator implements Serializable {
+	
+    private static final long serialVersionUID = 1L;
 
-	private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+    private String nome;
+    
+    @NotBlank
+    private String senha;
 
-	@JsonProperty("email")
-	@Column(nullable = false, length = 200)
-	private String email;
+    @NotBlank
+    @Column(unique = true)
+    @Email
+    private String email;
 
-	@JsonProperty("senha")
-	@Column(nullable = false, length = 200)
-	private String senha;
+    public Long getId() {
+        return id;
+    }
 
-	public long getId() {
-		return id;
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String getNome() {
+		return nome;
+	}
+    
+    public void setNome(String nome) {
+		this.nome = nome;
 	}
 
-	public void setId(long id) {
-		this.id = id;
+	@Override
+	public String toString() {
+		return "Administrador [id=" + id + ", nome=" + nome + ", senha=" + senha + ", email=" + email + "]";
 	}
 
-	public String getEmail() {
-		return email;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Administrator other = (Administrator) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-
+    
 }
